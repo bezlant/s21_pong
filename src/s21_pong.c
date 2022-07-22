b0bb6508919921f026b9ee121e239e7d880596a1
Anthony <89563512+bezlant@users.noreply.github.com>
2022-07-22 09:13:44 +0300

    Makefile implemented & Restructed project minorly (#1)

--- src/s21_pong.c
+++ src/s21_pong.c
@@ -1,38 +1,25 @@
-#include <curses.h>
-#include <time.h>
-
-#define MAX_X 80
-#define MIN_X 0
-#define MAX_Y 25
-#define MIN_Y 0
-
-typedef struct {
-    int x;
-    int y;
-    int score;
-    bool movY;
-    bool movX;
-} obj;
+#include "s21_pong.h"
 
 int main() {
-    initscr();          // curses start routine
+    initscr();  // curses start routine
     start_color();
     init_pair(1, COLOR_CYAN, COLOR_CYAN);
     init_pair(2, COLOR_RED, COLOR_BLACK);
     init_pair(3, COLOR_GREEN, COLOR_BLACK);
-    noecho();           // don't show getch() characters on the screen
-    curs_set(0);        // remove cursor
+    noecho();     // don't show getch() characters on the screen
+    curs_set(0);  // remove cursor
     nodelay(stdscr, true);
 
-    obj r2 = {MIN_X + 1, MAX_Y/2, 0, false, false};
-    obj r1 = {MAX_X - 2, MAX_Y/2, 0, false, false};
-    obj ball = {MAX_X/2, MAX_Y/2, 0, false, false};
+    obj r2 = {MIN_X + 1, MAX_Y / 2, 0, false, false};
+    obj r1 = {MAX_X - 2, MAX_Y / 2, 0, false, false};
+    obj ball = {MAX_X / 2, MAX_Y / 2, 0, false, false};
 
     while (r1.score < 21 && r2.score < 21) {
         clock_t start_t = clock();
         refresh();
 
-        while (clock() < start_t + 100000) {}
+        while (clock() < start_t + 100000) {
+        }
         // game logic
         if (ball.y == MAX_Y - 1 || ball.y == MIN_Y + 1)
             ball.movY = !ball.movY;
@@ -48,42 +35,52 @@ int main() {
                 } else {
                     r2.score++;
                 }
-                ball.x = MAX_X/2;
-                ball.y = MAX_Y/2;
+                ball.x = MAX_X / 2;
+                ball.y = MAX_Y / 2;
             }
         }
 
         // ball movement
 
-        if (ball.movX) ball.x++;
+        if (ball.movX)
+            ball.x++;
         else
             ball.x--;
-        if (ball.movY) ball.y++;
+        if (ball.movY)
+            ball.y++;
         else
             ball.y--;
 
-
         // slide through upper/lower border
-        if (r1.y <= MIN_Y + 1) r1.y = MAX_Y - 2;
-        if (r1.y >= MAX_Y - 1) r1.y = MIN_Y + 2;
-        if (r2.y <= MIN_Y + 1) r2.y = MAX_Y - 2;
-        if (r2.y >= MAX_Y - 1) r2.y = MIN_Y + 2;
+        if (r1.y <= MIN_Y + 1)
+            r1.y = MAX_Y - 2;
+        if (r1.y >= MAX_Y - 1)
+            r1.y = MIN_Y + 2;
+        if (r2.y <= MIN_Y + 1)
+            r2.y = MAX_Y - 2;
+        if (r2.y >= MAX_Y - 1)
+            r2.y = MIN_Y + 2;
 
         // key-presses
         int input = getch();
-        if (input == 'a') r2.y--;
-        if (input == 'z') r2.y++;
-        if (input == 'k') r1.y--;
-        if (input == 'm') r1.y++;
-        if (input == 27) break;
+        if (input == 'a')
+            r2.y--;
+        if (input == 'z')
+            r2.y++;
+        if (input == 'k')
+            r1.y--;
+        if (input == 'm')
+            r1.y++;
+        if (input == 27)
+            break;
         clear();
 
-        mvvline(MIN_Y, MAX_X/2, ACS_VLINE, MAX_Y);
+        mvvline(MIN_Y, MAX_X / 2, ACS_VLINE, MAX_Y);
         mvhline(MIN_Y, MIN_X, ACS_HLINE, MAX_X);
         mvhline(MAX_Y, MIN_X, ACS_HLINE, MAX_X);
 
         attron(COLOR_PAIR(3));
-        mvprintw(MIN_Y + 3, MAX_X/2 - 2, "%d - %d", r1.score, r2.score);
+        mvprintw(MIN_Y + 3, MAX_X / 2 - 2, "%d - %d", r1.score, r2.score);
         attroff(COLOR_PAIR(3));
 
         attron(COLOR_PAIR(2));
@@ -91,17 +88,17 @@ int main() {
         attroff(COLOR_PAIR(2));
 
         attron(COLOR_PAIR(1));
-        for (int i=-1; i < 2; i++) {
-            if (r1.y+i == MIN_Y || r1.y+i == MAX_Y) {
-                mvprintw(r2.y+i, r2.x, "|");
+        for (int i = -1; i < 2; i++) {
+            if (r1.y + i == MIN_Y || r1.y + i == MAX_Y) {
+                mvprintw(r2.y + i, r2.x, "|");
                 continue;
             }
-            if (r2.y+i == MIN_Y || r2.y+i == MAX_Y) {
-                mvprintw(r1.y+i, r1.x, "|");
+            if (r2.y + i == MIN_Y || r2.y + i == MAX_Y) {
+                mvprintw(r1.y + i, r1.x, "|");
                 continue;
             }
-                mvprintw(r2.y+i, r2.x, "|");
-                mvprintw(r1.y+i, r1.x, "|");
+            mvprintw(r2.y + i, r2.x, "|");
+            mvprintw(r1.y + i, r1.x, "|");
         }
         attroff(COLOR_PAIR(1));
     }
