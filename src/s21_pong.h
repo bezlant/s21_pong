b06818e8efb9ea54ec60f5d7b17f1f06731841b7
bezlant <abezludniy@mail.ru>
2022-07-22 09:00:25 +0300

    Makefile implemented & Restructed project minorly

---
+++ src/s21_pong.h
@@ -0,0 +1,16 @@
+
+#include <curses.h>
+#include <time.h>
+
+#define MAX_X 80
+#define MIN_X 0
+#define MAX_Y 25
+#define MIN_Y 0
+
+typedef struct {
+    int x;
+    int y;
+    int score;
+    bool movY;
+    bool movX;
+} obj;
